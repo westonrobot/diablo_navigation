fef0c59016a287716094cfd6bf28682b403bc097
Ruixiang Du <ruixiang.du@westonrobot.com>
2023-07-19 19:03:14 +0800

    diablo_robot: cleaned up main

--- diablo_base/src/main.cpp
+++ diablo_base/src/main.cpp
@@ -10,27 +10,9 @@
 #include <chrono>
 #include <iostream>
 
-#include "diablo_base/diablo_robot.hpp"
-
 #include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
-#include <nav_msgs/Odometry.h>
-#include <tf/tf.h>
-#include <tf/transform_broadcaster.h>
-#include <tf2/LinearMath/Matrix3x3.h>
-#include <tf2/LinearMath/Quaternion.h>
 
-#include "diablo_sdk/OSDK_LEGMOTORS.h"
-#include "diablo_utils/diablo_tools/osdk_vehicle.hpp"
-#include "diablo_utils/diablo_tools/onboard_sdk_uart_protocol.h"
-#include "diablo_base/coors.h"
-
-float right_speed_1, left_speed_1 = 0;
-float xcor, ycor, theta = 0;
-float radius = 0.09;
-float right_dist, left_dist, centre_dist = 0;
-float xold, yold, theta_old = 0;
-float duration;
+#include "diablo_base/diablo_robot.hpp"
 
 using namespace westonrobot;
 
